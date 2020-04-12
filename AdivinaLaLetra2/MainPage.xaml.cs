0850c39b280dcc8af568675bc10d626f2133ff65
Oscar Quesada <oscaardanielqc@outlook.es>
2020-04-12 16:27:03 -0600

    COmmit 1

---
+++ AdivinaLaLetra2/MainPage.xaml.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Windows.Markup.Localizer;
+using System.Collections;
+using System.Web;
+
+namespace AdivinaLaLetra2
+{
+ 
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    
+    public partial class MainPage : ContentPage
+    {
+       
+        public static string ranS = RandomString();
+        public static string ranS2 = ranS + ranS;
+        public static string ranS1 = Shuffle(ranS2);
+        Label labelResult = new Label();
+        List<char> termsList = new List<char>();
+       List<Button> GridButtons = new List<Button>();
+        int[,] PossArray;
+        public string ViewResult;
+        public string OldBtnName;
+
+        public bool correcto;
+        public MainPage()
+        {
+           
+            InitializeComponent();
+            Lisbuttons();
+
+        
+
+        }
+
+        public void Lisbuttons() {
+           
+
+            {
+                GridButtons.Add(LetterBtn1); GridButtons.Add(LetterBtn13);
+                GridButtons.Add(LetterBtn2); GridButtons.Add(LetterBtn14);
+                GridButtons.Add(LetterBtn3); GridButtons.Add(LetterBtn15);
+                GridButtons.Add(LetterBtn4); GridButtons.Add(LetterBtn16);
+                GridButtons.Add(LetterBtn5); GridButtons.Add(LetterBtn17);
+                GridButtons.Add(LetterBtn6); GridButtons.Add(LetterBtn18);
+                GridButtons.Add(LetterBtn7); GridButtons.Add(LetterBtn19);
+                GridButtons.Add(LetterBtn8); GridButtons.Add(LetterBtn20);
+                GridButtons.Add(LetterBtn9); GridButtons.Add(LetterBtn21);
+                GridButtons.Add(LetterBtn10); GridButtons.Add(LetterBtn22);
+                GridButtons.Add(LetterBtn11); GridButtons.Add(LetterBtn23);
+                GridButtons.Add(LetterBtn12); GridButtons.Add(LetterBtn24);
+            }
+        }
+        
+        public static string Shuffle(string str)
+        {
+            char[] array = str.ToCharArray();
+            try
+            {
+               
+                Random rng = new Random();
+                int n = array.Length;
+                while (n > 1)
+                {
+                    n--;
+                    int k = rng.Next(n + 1);
+                    var value = array[k];
+                    array[k] = array[n];
+                    array[n] = value;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return new string(array);
+        }
+
+
+
+        public char RandomChar()
+        {
+            Random rnd = new Random();
+            char randomChar = (char)rnd.Next('a', 'z');
+            return randomChar;
+        }
+
+        static string RandomString()
+        {
+
+            List<string> randStr = new List<string>();
+            try
+            {
+                Random random = new Random();
+                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                
+                for (int i = 0; i <= 11; i++)
+                {
+                    string AlphaRandom = new string(Enumerable.Repeat(chars, 1)
+                      .Select(s => s[random.Next(s.Length)]).ToArray());
+
+
+
+                    if (randStr.Contains(AlphaRandom))
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        randStr.Add(AlphaRandom);
+
+
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            string rs = string.Join("", randStr.ToArray());
+            return rs;
+        }
+
+        private System.Data.DataSet dataSet;
+
+     
+
+        public void Grid1()
+        {
+
+
+            try
+            {
+                
+                var darkerButton = new Style(typeof(Button))
+                {
+                    Setters = {
+                  new Setter { Property = Button.BackgroundColorProperty, Value = Color.FromHex ("#ddd") },
+                  new Setter { Property = Button.TextColorProperty, Value = Color.Black },
+                  new Setter { Property = Button.BorderRadiusProperty, Value = 0 },
+                  new Setter { Property = Button.FontSizeProperty, Value = 15 }
+                }
+                };
+                
+
+                Grid controlGrid  = new Grid{ RowSpacing = 1, ColumnSpacing = 1 };
+                controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+                controlGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                controlGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                controlGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                controlGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+               
+                controlGrid.Children.Add(labelResult, 0, 0);
+
+                Grid.SetColumnSpan(labelResult, 4);
+
+
+
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 0, 1);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 1, 1);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 2, 1);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 3, 1);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 4, 1);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 0, 2);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 1, 2);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 2, 2);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 3, 2);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 4, 2);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 0, 3);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 1, 3);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 2, 3);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 3, 3);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 4, 3);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 0, 4);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 1, 4);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 2, 4);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 3, 4);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 4, 4);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 0, 5);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 1, 5);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 2, 5);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 3, 5);
+                controlGrid.Children.Add(new Button { Text = "", Style = plainButton }, 4, 5);
+
+
+                Content = controlGrid;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        
+        void ShowLabel()
+        {
+            labelResult = new Label
+            {
+                Text = ViewResult,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                TextColor = Color.Black,
+                FontSize = 25
+                
+            };
+            
+        }
+       
+        
+            Style orangeButton = new Style(typeof(Button))
+            {
+                Setters =
+                {
+                      new Setter { Property = Button.BackgroundColorProperty, Value = Color.FromHex ("#E8AD00") },
+                      new Setter { Property = Button.TextColorProperty, Value = Color.White },
+                      new Setter { Property = Button.BorderRadiusProperty, Value = 0 },
+                      new Setter { Property = Button.FontSizeProperty, Value = 25 }
+                   
+                }
+            };
+
+            Style plainButton = new Style(typeof(Button))
+            {
+                Setters = {
+                      new Setter { Property = Button.BackgroundColorProperty, Value = Color.FromHex ("#eee") },
+                      new Setter { Property = Button.TextColorProperty, Value = Color.Black },
+                      new Setter { Property = Button.BorderRadiusProperty, Value = 0 },
+                      new Setter { Property = Button.FontSizeProperty, Value = 15 }
+                    }
+            };
+        public void GetPositionButton(System.Object sender)
+        {
+            try
+            {
+                int row = Grid.GetRow(sender as Button);
+                int column = Grid.GetColumn(sender as Button);
+                PossArray = new[,] { { row },{ column } };
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        void ShowVar(int c, System.Object sender)
+        {
+            try
+            {
+                (sender as Button).Text = char.ToString(ranS1[c]);
+                (sender as Button).Style = orangeButton;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        void ShowBlankButton(System.Object sender)
+        {
+            try
+            {
+                (sender as Button).Text = "";
+                (sender as Button).Style = plainButton;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+            {
+
+
+            try
+            {
+                var button = (Button)sender;
+                string classId = button.ClassId;
+                string name = button.AutomationId.ToString();
+               
+                int c = Int32.Parse(classId);
+                ShowVar(c,(sender as Button));
+               
+                termsList.Add(ranS1[c]);
+               
+
+
+               if (termsList.Count.Equals(0))
+                {
+                    ViewResult = "Adivina la Letra";
+                }
+                else if (termsList.Last().Equals(c))
+                {
+                    ViewResult = "Correcto!";
+                    (sender as Button).IsEnabled = false;
+
+                }
+                else{
+                    foreach (var item in GridButtons)
+                    {
+                        if (item.AutomationId.Equals(OldBtnName))
+                        {
+                            ShowBlankButton(item);
+                        }
+                    }
+
+                }
+                
+                ShowLabel();
+                OldBtnName = name;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            }
+
+        void enableAllButtons()
+        {
+           
+        }
+
+
+    }
+}
+
+
